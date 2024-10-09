93df7f6cf17d516af20eed36b341f6c666261a0b
NguyenVanPhuoc1 <nguyenvanphuoc031123@gmail.com>
2024-10-09 22:04:25 +0700

    update OOP with dependency_injection

--- vehiclebooking_be/app/Http/Controllers/FilterController.php
+++ vehiclebooking_be/app/Http/Controllers/FilterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Cars;
 use App\Http\Controllers\ProductController;
+use App\Service\CarService;
 
 class FilterController extends Controller
 {
@@ -50,51 +51,18 @@ class FilterController extends Controller
 
     //     return $query;
     // }
+    protected $carService;
 
+    public function __construct(CarService $carService)
+    {
+        $this->carService = $carService;
+    }
     public function _getDataFilter(Request $request)
     {
         // Lấy các tham số lọc từ yêu cầu
         $filters = $request->query('params', []);
 
-        // Khởi tạo truy vấn cho mô hình Car
-        $query = Cars::query();
-        // Áp dụng các bộ lọc vào truy vấn
-        if (isset($filters['brand_id']) && is_array($filters['brand_id'])) {
-            $query->whereIn('brand_id', $filters['brand_id']);
-        }
-
-        if (isset($filters['priceRange']) && is_array($filters['priceRange'])) {
-            $minPrice = isset($filters['priceRange'][0]) ? floatval($filters['priceRange'][0]) : null;
-            $maxPrice = isset($filters['priceRange'][1]) ? floatval($filters['priceRange'][1]) : null;
-
-            if ($minPrice !== null) {
-                $query->where('price_per_day', '>=', $minPrice);
-            }
-
-            if ($maxPrice !== null) {
-                $query->where('price_per_day', '<=', $maxPrice);
-            }
-        }
-        $query->whereHas('specification', function($q) use ($filters, &$seat) {
-            if (isset($filters['body_name'])) {
-                $q->whereIn('body', $filters['body_name']);
-            }
-        
-            if (isset($filters['car_seats'])) {
-                $seat = array_map('floatval', $filters['car_seats']);;
-                $q->whereIn('seat', $seat);
-            }
-        });
-
-        // Lấy danh sách các xe theo bộ lọc
-        // $cars = $query->with(['specification', 'getImageCars'])->get();
-        $cars = [];
-        $query->with(['specification', 'getImageCars'])->chunk(100, function ($chunk) use (&$cars) {
-            // Thêm mỗi lô kết quả vào mảng $cars
-            foreach ($chunk as $car) {
-                $cars[] = $car;
-            }
-        });
+        $cars = $this->carService->getFilteredCars($filters);
         // Trả về kết quả dưới dạng JSON
         return response()->json($cars);
     }
