ef2e9b740b9adadb4be69867657530c47c6488cf
Nguyễn Văn Phước <145408378+NguyenVanPhuoc1@users.noreply.github.com>
2024-10-13 01:31:03 +0700

    Update database.php

--- vehiclebooking_be/config/database.php
+++ vehiclebooking_be/config/database.php
@@ -94,22 +94,22 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
-        // 'mongodb' => [
-        //     'driver' => 'mongodb',
-        //     'host' => env('DB_HOST', 'localhost'),
-        //     'port' => env('DB_PORT', 27017),
-        //     'database' => env('DB_DATABASE', 'wecommit'),
-        //     'username' => env('DB_USERNAME', ''),
-        //     'password' => env('DB_PASSWORD', ''),
-        //     'options' => [
-        //         'database' => env('DB_AUTHENTICATION_DATABASE', 'users')
-        //     ],
-        // ],
         'mongodb' => [
             'driver' => 'mongodb',
-            'uri' => env('MONGODB_URL', 'mongodb://localhost:27017'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE', 'wecommit'),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'options' => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'users')
+            ],
         ],
+        // 'mongodb' => [
+        //     'driver' => 'mongodb',
+        //     'uri' => env('MONGODB_URL', 'mongodb://localhost:27017'),
+        //     'database' => env('DB_DATABASE', 'wecommit'),
+        // ],
 
     ],
 
